2f4bc95d61b9feea7e68ca66e47cd2ddc2f15c32
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-01-29 22:31:20 +0530

    score keeper

---
+++ app.js
@@ -0,0 +1,10 @@
+const p1Button = document.querySelector("#p1Button");
+const p2Button = document.querySelector("#p2Button");
+const p1Display = document.querySelector("#p1Display");
+const p2Display = document.querySelector("#p2Display");
+let p1Score = 0;
+
+p1Button.addEventListener("click", function () {
+    p1Score += 1;
+    p1Display.innerText = p1Score;
+})
