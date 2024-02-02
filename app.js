7bb0f643471db9bddfdc92eec551cf275da0f76f
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-02-02 22:48:04 +0530

    score keeper

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ p1Button.addEventListener("click", function () {
         p1Score += 1;
         if (p1Score === winningScore) {
             isGameOver = true;
-            p1Display.classList.add("winner");
-            p2Display.classList.add("loser");
+            p1Display.classList.add("has-text-success");
+            p2Display.classList.add("has-text-danger");
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p1Display.innerText = p1Score;
     }
@@ -27,8 +29,10 @@ p2Button.addEventListener("click", function () {
         p2Score += 1;
         if (p2Score === winningScore) {
             isGameOver = true;
-            p2Display.classList.add("winner");
-            p1Display.classList.add("loser");
+            p2Display.classList.add("has-text-success");
+            p1Display.classList.add("has-text-danger");
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.innerText = p2Score;
     }
@@ -47,8 +51,10 @@ function resetEverything() {
     p1Display.innerText = p1Score;
     p2Display.innerText = p2Score;
     isGameOver = false;
-    p1Display.classList.remove("winner", "loser");
-    p2Display.classList.remove("winner", "loser");
+    p1Display.classList.remove("has-text-success", "has-text-danger");
+    p2Display.classList.remove("has-text-success", "has-text-danger");
+    p1Button.disabled = false;
+    p2Button.disbaled = false;
 
 }
 
